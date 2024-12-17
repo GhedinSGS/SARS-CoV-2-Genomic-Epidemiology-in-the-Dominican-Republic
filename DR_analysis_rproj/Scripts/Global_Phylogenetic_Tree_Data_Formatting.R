f0648fb3c920258b000f3d56b61fe21492f23ae9
alliekreitman <allie.kreitman@gmail.com>
2024-12-17 14:44:54 -0800

    Update Global_Phylogenetic_Tree_Data_Formatting.R

--- DR_analysis_rproj/Scripts/Global_Phylogenetic_Tree_Data_Formatting.R
+++ DR_analysis_rproj/Scripts/Global_Phylogenetic_Tree_Data_Formatting.R
@@ -19,7 +19,7 @@ library(ape)
 # tree
 tree <- read.newick("Data_Reanalysis/ncov_results/global_tree/tree.nwk")
 # metadata
-metadata_raw <- read.csv("Data_Reanalysis/sample_data/DR_metadata_with_lineage.csv")
+metadata_raw <- read_tsv("Data_Reanalysis/ncov_results/global_tree/metadata_with_nextclade_qc.tsv")
 
 #samples in tree
 samples_in_tree <- tree$tip.label
@@ -27,32 +27,13 @@ samples_in_tree <- tree$tip.label
 #format metadata
 metadata <- metadata_raw %>% 
   # remove samples filtered out
-  filter(samplename %in% samples_in_tree) %>% 
+  filter(strain %in% samples_in_tree) %>% 
   #select relevant strains
-  select(samplename, date, region, country, gisaid_epi_isl, Nextstrain_clade, Nextclade_pango) %>% 
-  #change samples for this study gisaid_epi_isl number to say "SGS"
-  mutate(gisaid_epi_isl = ifelse(is.na(gisaid_epi_isl)==TRUE, samplename, gisaid_epi_isl)) %>% 
-  # add strain rename column
-  mutate(strain_renamed = paste0(gsub(" ", "", country), "/", gsub("-", "", date), "/", gsub("_", "", gisaid_epi_isl)))
-
-# rename tree
-tree_renamed <- rename_taxa(tree = tree, data = metadata, key = strain, value = strain_renamed)
-
-# remove old strain name from metadata
-metadata <- metadata %>% 
-  #remove old strain name
-  select(-strain) %>% 
-  # renamed new strain to just be called strain
-  rename(strain = strain_renamed) %>% 
-  # remove 3 repeat samples
-  filter(!strain %in% samples_to_drop)
+  select(strain, date, region, country, Nextstrain_clade, Nextclade_pango) 
 
 metadata <- metadata %>% 
-  mutate(GL_yn = ifelse(grepl("EPI_ISL_", gisaid_epi_isl)==FALSE, "y", "n"))
-
-#fully align names - not sure why this is necessary but it is buggy without! 
-metadata <- left_join(tree_labels, metadata, by="strain")
+  mutate(GL_yn = ifelse(grepl("^DR[0-9]{4}$", strain) == T, "y", "n"))
 
 #clean workspace
-rm(tree, metadata_raw, samples_in_tree)
+rm(metadata_raw, samples_in_tree)
 
